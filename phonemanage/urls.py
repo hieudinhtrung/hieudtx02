2d2fc285b3416a98bab620a71422843106edc0d1
khacthanha2 <khacthanha2@gmail.com>
2021-10-30 16:58:22 +0700

    Apply template

---
+++ phonemanage/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from phonemanage import views
+
+urlpatterns = [
+    path ('login/', views.login, name='login'),
+    path ('dashboard/', views.dashboard, name='dashboard')
+]
+
