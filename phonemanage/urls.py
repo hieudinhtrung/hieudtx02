135dd3842197d7f7cd6c97322464c7dd294f0184
tuananh <tuananh@gmail.com>
2022-01-12 16:23:43 +0700

    depart_ex

--- phonemanage/urls.py
+++ phonemanage/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
      path('edit_contact/<int:pk>', views.edit_contact, name="edit_contact"),
      path('delete_contact/<int:pk>', views.delete_contact, name="delete_contact"),
      path('show_contact/<int:pk>', views.show_contact, name="show_contact"),
-     path('import_contact/', views.impo, name="import_contact")
+     path('import_contact/', views.impo, name="import_contact"),
+     path('contact/listcontact_ex/', views.listcontact_ex,name= 'listcontact_ex'),
+     path('add_contact/', views.create_contact_ex, name="create_contact_ex"),
+     path('edit_contact/<int:pk>', views.edit_contact_ex, name="edit_contact_ex"),
+     path('delete_contact/<int:pk>', views.delete_contact_ex, name="delete_contact_ex"),
+     path('show_contact/<int:pk>', views.show_contact_ex, name="show_contact_ex"),
+     path('import_contact/', views.impo_ex, name="import_contact_ex")
 ]
